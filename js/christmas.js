0106959724e51fd4b32dbe9ea800e2a5c38255c9
feh-stuff <feh-stuff@openmailbox.org>
2017-12-25 20:04:30 +0800

    Add Christmas presents

---
+++ js/christmas.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  const code = 'christmas2017';
+
+  if (JSON.parse(localStorage.getItem(code))) {
+    return;
+  }
+
+  let $modal = $(`<div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" style="margin-top:10%;" role="document">
+      <div class="modal-content bg-danger text-white">
+        <div class="modal-header" style="border-bottom:0;">
+          <h5 class="modal-title">Merry Christmas!</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body d-flex justify-content-around">
+          <div class="text-center">
+            <img src="img/assets/orbs.png">
+            <h2>1</h2>
+          </div>
+          <div class="text-center">
+            <img src="img/assets/feather.png">
+            <h2>100</h2>
+          </div>
+        </div>
+        <div class="modal-footer" style="border-top:0;">
+          <button type="button" id="accept-all" class="btn btn-success btn-block" data-dismiss="modal">Accept All</button>
+        </div>
+      </div>
+    </div>
+  </div>`).appendTo('body');
+
+  $modal.modal('show');
+  $modal.on('click', '#accept-all', function() {
+    localStorage.setItem(code, 'true');
+  });
+});
